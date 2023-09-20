6365babc6ba4b7f65d9379062f0990aa2bbc3efb
jhennies <julianhennies@hotmail.de>
2023-09-20 13:48:20 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='squirrel',
+    version='0.0.1',
+    author='jhennies',
+    author_email='hennies@embl.de',
+    packages=['squirrel'],
+    scripts=[]
+)
