61786392a3cc8e16ff4037592e2a40b8e09e85bd
jhennies <julianhennies@hotmail.de>
2024-07-12 22:12:22 +0200

    volume padding function

--- squirrel/library/volume.py
+++ squirrel/library/volume.py
@@ -2,12 +2,23 @@
 import numpy as np
 
 
-def pad_volume(vol, min_shape):
+def pad_volume(vol, min_shape, axes=None):
 
     if np.all(np.array(vol.shape) > np.array(min_shape)):
         return vol
 
-    t_vol = np.zeros(np.max([vol.shape, min_shape], axis=0), dtype=vol.dtype)
+    if axes is None:
+
+        new_shape = np.max([vol.shape, min_shape], axis=0)
+
+    else:
+        new_shape = vol.shape
+        for a in axes:
+            if vol.shape[a] < min_shape[a]:
+                new_shape[a] = min_shape[a]
+
+    t_vol = np.zeros(new_shape, dtype=vol.dtype)
     t_vol[:vol.shape[0], :vol.shape[1], :vol.shape[2]] = vol
 
     return t_vol
+
