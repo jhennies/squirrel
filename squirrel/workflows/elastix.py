961cf8e5c0c591f0d75fd6a9a27f901a36b42f53
jhennies <julianhennies@hotmail.de>
2024-08-08 17:50:58 +0200

    bug fix in auto_padding

--- squirrel/workflows/elastix.py
+++ squirrel/workflows/elastix.py
@@ -489,9 +489,9 @@ def apply_multi_step_stack_alignment_workflow(
                 stack = stack.new_stack_with_same_meta(stack[start_transform_id: start_transform_id + z_range[1] - z_range[0]])
             if stack.exists_meta('stack_shape'):
                 image_shape = stack.get_meta('stack_shape')[1:]
+                target_image_shape = image_shape
             else:
                 image_shape = target_image_shape
-                target_image_shape = image_shape
             if not stack.is_sequenced:
                 stack = stack.get_sequenced_stack()
             stacks.append(ElastixStack(stack=stack, image_shape=image_shape))
