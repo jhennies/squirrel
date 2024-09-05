b39b2d519b2bc0cbb17403ac42961ed7ae6dcbfc
jhennies <julianhennies@hotmail.de>
2024-09-05 22:45:20 +0200

    back to xcorr for validation

--- squirrel/library/xcorr.py
+++ squirrel/library/xcorr.py
@@ -1,13 +1,13 @@
 
 def xcorr(fixed, moving, sigma=1.0):
-    # from vigra.filters import gaussianGradientMagnitude
+    from vigra.filters import gaussianGradientMagnitude
     from vigra.filters import gaussianSmoothing
     from skimage.registration import phase_cross_correlation
 
-    # fixed_ = gaussianGradientMagnitude(fixed.astype('float32'), sigma)
-    # moving_ = gaussianGradientMagnitude(moving.astype('float32'), sigma)
-    fixed_ = gaussianSmoothing(fixed.astype('float32'), sigma)
-    moving_ = gaussianSmoothing(moving.astype('float32'), sigma)
+    fixed_ = gaussianGradientMagnitude(fixed.astype('float32'), sigma)
+    moving_ = gaussianGradientMagnitude(moving.astype('float32'), sigma)
+    # fixed_ = gaussianSmoothing(fixed.astype('float32'), sigma)
+    # moving_ = gaussianSmoothing(moving.astype('float32'), sigma)
 
     return phase_cross_correlation(
         fixed_, moving_, upsample_factor=100
