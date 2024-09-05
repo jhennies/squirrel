22b2c2a34302ecdb6f6f37f82b07f2f21b664f56
jhennies <julianhennies@hotmail.de>
2024-09-05 21:32:44 +0200

    adding sift for validation

--- squirrel/library/sift2d.py
+++ squirrel/library/sift2d.py
@@ -2,6 +2,43 @@
 import numpy as np
 
 
+def register_with_sift2(
+        fixed_image,
+        moving_image,
+        verbose=False
+):
+
+    import cv2
+
+    sift = cv2.SIFT_create(nOctaveLayers=3, sigma=1.6, contrastThreshold=0.09)
+
+    kp_img, des_img = sift.detectAndCompute(moving_image, None)
+    kp_ref, des_ref = sift.detectAndCompute(fixed_image, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+    matches = bf.match(des_img, des_ref)
+
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    img_matches = cv2.drawMatches(moving_image, kp_img, fixed_image, kp_ref, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    # Compute the offset by analyzing matched keypoints
+    if len(img_matches) > 0:
+        # Extract location of good matches
+        points1 = np.float32([kp_img[m.queryIdx].pt for m in img_matches])
+        points2 = np.float32([kp_ref[m.trainIdx].pt for m in img_matches])
+
+        # Find translation vector using mean of the differences
+        offset = np.median(points2 - points1, axis=0)
+        if verbose:
+            print(f"Offset between images: {offset}")
+    else:
+        print("Not enough matches found.")
+        offset = [0., 0.]
+
+    return offset
+
+
 def register_with_sift(
         fixed_image,
         moving_image,
