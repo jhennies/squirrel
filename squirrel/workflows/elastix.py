4f7e572f827f37b60ad6b7f255ee5dc19988b8d0
jhennies <julianhennies@hotmail.de>
2024-09-05 22:00:51 +0200

    adding sift for validation

--- squirrel/workflows/elastix.py
+++ squirrel/workflows/elastix.py
@@ -450,9 +450,9 @@ def stack_alignment_validation_workflow(
                     result_matrix = AffineMatrix(parameters=[1, 0, shift[0], 0, 1, shift[1]])
 
                 elif method == 'sift':
-                    result_matrix = register_with_sift(
+                    result_matrix = AffineMatrix(parameters=register_with_sift(
                         z_slice_fixed, z_slice_moving  # , transform='translation'
-                    )
+                    ))
 
                 if verbose:
                     print(f'result_matix = {result_matrix}')
