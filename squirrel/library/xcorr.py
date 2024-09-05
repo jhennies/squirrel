ffc23342e4ef6d09f2e06319ef7ca9c7fca80a63
jhennies <julianhennies@hotmail.de>
2024-09-05 17:46:29 +0200

    improving xcorr for validation

--- squirrel/library/xcorr.py
+++ squirrel/library/xcorr.py
@@ -3,8 +3,8 @@ def xcorr(fixed, moving, sigma=1.0):
     from vigra.filters import gaussianGradientMagnitude
     from skimage.registration import phase_cross_correlation
 
-    fixed_ = gaussianGradientMagnitude(fixed, sigma)
-    moving_ = gaussianGradientMagnitude(moving, sigma)
+    fixed_ = gaussianGradientMagnitude(fixed.astype('float32'), sigma)
+    moving_ = gaussianGradientMagnitude(moving.astype('float32'), sigma)
 
     return phase_cross_correlation(
         fixed_, moving_, upsample_factor=10
