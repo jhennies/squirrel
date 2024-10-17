f4b90b8b7728643161830e9e5bd6cf8d036bf454
jhennies <julianhennies@hotmail.de>
2024-10-17 13:41:40 +0200

    Mobie table project

--- squirrel/library/table_mobie.py
+++ squirrel/library/table_mobie.py
@@ -1,3 +1,5 @@
+import os.path
+
 
 def get_mobie_table_path(project_path):
     return os.path.join(project_path, 'mobie.csv')
@@ -54,4 +56,33 @@ def update_mobie_table_entry(table_filepath, entry, item):
     mobie_table.at[row_index, entry[0]] = entry[1]
 
     mobie_table.to_csv(table_filepath, index=False, sep='\t')
+
+
+def init_mobie_table(table_filepath, data_map_filepaths, verbose=False):
+    if verbose:
+        print(f'table_filepath = {table_filepath}')
+        print(f'data_map_filepaths = {data_map_filepaths}')
+
+
+class MobieTableProject():
+
+    def __init__(self, project_dirpath, verbose=False):
+
+        self._project_dirpath = project_dirpath
+        self._verbose = verbose
+
+        if not os.path.exists(project_dirpath):
+            os.mkdir(project_dirpath)
+            return
+
+    def init_with_data_maps(self, data_map_filepaths):
+        assert len(os.listdir(self._project_dirpath)) == 0, 'Can only initialize a project in an empty directory'
+
+        self._mobie_table_path = get_mobie_table_path(self._project_dirpath)
+        init_mobie_table(self._mobie_table_path, data_map_filepaths, verbose=self._verbose)
+
+    def get_project_dirpath(self):
+        return self._project_dirpath
+
+
 
