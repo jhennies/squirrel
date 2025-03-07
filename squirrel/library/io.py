818f65083f82912bbd4f50748641653b6538934e
jhennies <julianhennies@hotmail.de>
2025-03-07 14:39:57 +0100

    Mobie export from table bounding box

--- squirrel/library/io.py
+++ squirrel/library/io.py
@@ -234,7 +234,7 @@ class TiffStack(list):
         list.__init__(self, stack)
         self.dtype = self[0].dtype
         self.shape = self.get_shape()
-        self.chunks = [1] + self.shape[2:]
+        self.chunks = [1] + self.shape[1:]
 
     def __getitem__(self, item):
 
