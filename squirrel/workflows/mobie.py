49c0c557728410ad4a108c886589c8477b26c3dc
jhennies <julianhennies@hotmail.de>
2025-03-07 21:41:09 +0100

    Mobie export from table bounding box

--- squirrel/workflows/mobie.py
+++ squirrel/workflows/mobie.py
@@ -3,6 +3,79 @@ import os
 import numpy as np
 
 
+def _get_position_px(table, idx, res):
+    row = table.loc[idx]
+    x = row['bb_min_x']
+    y = row['bb_min_y']
+    z = row['bb_min_z']
+    w = row['bb_max_x'] - x
+    h = row['bb_max_y'] - y
+    d = row['bb_max_z'] - z
+    return np.array((z, y, x)) / np.array(res), np.array((d, h, w)) / np.array(res)
+
+
+def _get_data(data_h, idx, resolution, table, verbose=False):
+    zyx, dhw = _get_position_px(table, idx, resolution)
+    z, y, x = np.array(zyx).astype(int)
+    d, h, w = np.array(dhw).astype(int)
+
+    if verbose:
+        print(f'zyx = {zyx}')
+        print(f'dhw = {dhw}')
+
+    return data_h[z:z + d, y:y + h, x:x + w], x, y, z
+
+
+def _apply_mask(map_data, mask_h, idx, map_resolution, mask_resolution, table, verbose=False):
+    mask_data, mx, my, mz = _get_data(mask_h, idx, mask_resolution, table, verbose=verbose)
+
+    if mask_resolution != map_resolution:
+        from squirrel.library.scaling import scale_image_nearest
+        mask_data = scale_image_nearest(mask_data, np.array(mask_resolution) / np.array(map_resolution))
+
+    print(f'map_data.shape = {map_data.shape}')
+    print(f'mask_data.shape = {mask_data.shape}')
+    mask_data_pad = np.zeros(map_data.shape)
+    mask_data_pad[:mask_data.shape[0], :mask_data.shape[1], :mask_data.shape[2]] = mask_data
+    map_data[mask_data_pad != idx] = 0
+    return map_data
+
+
+def _cast_dtype(map_data):
+    from squirrel.library.data import get_optimal_dtype
+    print(f'Casting dtype:')
+    print(f'Getting label mapping ...')
+    label_list = np.unique(map_data)
+    label_mapping = dict(zip(label_list, range(len(label_list))))
+    print(f'Finding optimal dtype ...')
+    dtype = get_optimal_dtype(len(label_list))
+    print(f'Mapping the data ...')
+    # map_func = np.vectorize(label_mapping.get)
+    # map_data = map_func(map_data).astype(dtype)
+    for sl_idx, sl in enumerate(map_data):
+        print(f'sl_idx = {sl_idx}')
+        u = np.unique(sl)
+        if len(u) > 1 or u[0] != 0:
+            lm = {key: label_mapping[key] for key in list(u) if key in label_mapping}
+            map_func = np.vectorize(lm.get)
+            map_data[sl_idx, :] = map_func(sl)
+    return map_data.astype(dtype)
+
+
+def _run_for_label_id(idx, map_h, mask_h, map_resolution, mask_resolution, write_func, table, verbose=False):
+    map_data, x, y, z = _get_data(map_h, idx, map_resolution)
+
+    if verbose:
+        print(f'data.shape = {map_data.shape}')
+
+    if mask_h is not None:
+        map_data = _apply_mask(map_data, mask_h, idx, map_resolution, mask_resolution, table, verbose=verbose)
+
+    map_data = _cast_dtype(map_data)
+
+    write_func(map_data, idx, x, y, z)
+
+
 def export_rois_with_mobie_table_workflow(
         table_filepath,
         map_dirpath,
@@ -37,16 +110,6 @@ def export_rois_with_mobie_table_workflow(
         table.set_index('label_id', inplace=True)
         return table
 
-    def _get_position_px(table, idx, res):
-        row = table.loc[idx]
-        x = row['bb_min_x']
-        y = row['bb_min_y']
-        z = row['bb_min_z']
-        w = row['bb_max_x'] - x
-        h = row['bb_max_y'] - y
-        d = row['bb_max_z'] - z
-        return np.array((z, y, x)) / np.array(res), np.array((d, h, w)) / np.array(res)
-
     write_func = None
     if output_filetype == 'tif':
         from squirrel.library.io import write_tif_stack
@@ -69,71 +132,11 @@ def export_rois_with_mobie_table_workflow(
     if mask_dirpath is not None:
         mask_h, mask_shape = load_data_handle(mask_dirpath, key=mask_key, pattern=None)
 
-    def _get_data(data_h, idx, resolution):
-        zyx, dhw = _get_position_px(table, idx, resolution)
-        z, y, x = np.array(zyx).astype(int)
-        d, h, w = np.array(dhw).astype(int)
-
-        if verbose:
-            print(f'zyx = {zyx}')
-            print(f'dhw = {dhw}')
-
-        return data_h[z:z+d, y:y+h, x:x+w], x, y, z
-
-    def _apply_mask(map_data, mask_h, idx, mask_resolution):
-
-        mask_data, mx, my, mz = _get_data(mask_h, idx, mask_resolution)
-
-        if mask_resolution != map_resolution:
-            from squirrel.library.scaling import scale_image_nearest
-            mask_data = scale_image_nearest(mask_data, np.array(mask_resolution) / np.array(map_resolution))
-
-        print(f'map_data.shape = {map_data.shape}')
-        print(f'mask_data.shape = {mask_data.shape}')
-        mask_data_pad = np.zeros(map_data.shape)
-        mask_data_pad[:mask_data.shape[0], :mask_data.shape[1], :mask_data.shape[2]] = mask_data
-        map_data[mask_data_pad != idx] = 0
-        return map_data
-
-    def _cast_dtype(map_data):
-        from squirrel.library.data import get_optimal_dtype
-        print(f'Casting dtype:')
-        print(f'Getting label mapping ...')
-        label_list = np.unique(map_data)
-        label_mapping = dict(zip(label_list, range(len(label_list))))
-        print(f'Finding optimal dtype ...')
-        dtype = get_optimal_dtype(len(label_list))
-        print(f'Mapping the data ...')
-        # map_func = np.vectorize(label_mapping.get)
-        # map_data = map_func(map_data).astype(dtype)
-        for sl_idx, sl in enumerate(map_data):
-            print(f'sl_idx = {sl_idx}')
-            u = np.unique(sl)
-            if len(u) > 1 or u[0] != 0:
-                lm = {key: label_mapping[key] for key in list(u) if key in label_mapping}
-                map_func = np.vectorize(lm.get)
-                map_data[sl_idx, :] = map_func(sl)
-        return map_data.astype(dtype)
-
-    def _run_for_label_id(idx, map_h, mask_h, map_resolution, mask_resolution):
-
-        map_data, x, y, z = _get_data(map_h, idx, map_resolution)
-
-        if verbose:
-            print(f'data.shape = {map_data.shape}')
-
-        if mask_h is not None:
-            map_data = _apply_mask(map_data, mask_h, idx, mask_resolution)
-
-        map_data = _cast_dtype(map_data)
-
-        write_func(map_data, idx, x, y, z)
-
     n_workers = 2
 
     if n_workers == 1:
         for idx in label_ids:
-            _run_for_label_id(idx, map_h, mask_h, map_resolution, mask_resolution)
+            _run_for_label_id(idx, map_h, mask_h, map_resolution, mask_resolution, write_func, table, verbose=verbose)
 
     else:
 
@@ -142,7 +145,7 @@ def export_rois_with_mobie_table_workflow(
             tasks = [
                 p.apply_async(
                     _run_for_label_id,
-                    (idx, map_h, mask_h, map_resolution, mask_resolution)
+                    (idx, map_h, mask_h, map_resolution, mask_resolution, write_func, table, verbose)
                 )
                 for idx in label_ids
             ]
