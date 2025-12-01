7ce11920e14e63a69fbd4614532b98160dbc2246
jhennies <julianhennies@hotmail.de>
2025-12-01 16:34:52 +0100

    v0.3.11: affine matrices, fixing rigid transform for elastix inputs

--- squirrel/library/affine_matrices.py
+++ squirrel/library/affine_matrices.py
@@ -525,7 +525,7 @@ class AffineMatrix:
                 parameters[0] = 'translation'
         assert isinstance(parameters[0], str), \
             'Elastix parameters must be in the format: ["transform", [parameters, ...]]'
-        assert parameters[0] in ['translation', 'TranslationTransform', 'rigid', 'SimilarityTransform', 'affine', 'AffineTransform'], f'invalid transform: {parameters[0]}'
+        assert parameters[0] in ['translation', 'TranslationTransform', 'rigid', 'EulerTransform', 'SimilarityTransform', 'affine', 'AffineTransform'], f'invalid transform: {parameters[0]}'
         from ..library.elastix import elastix_to_c
         parameters = elastix_to_c(*parameters)
         self.set_from_parameters(parameters, pivot=pivot)
