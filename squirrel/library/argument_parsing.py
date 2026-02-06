794bb53b96ec8d86b848540686c07b9e1ff3d038
jhennies <julianhennies@hotmail.de>
2026-02-06 09:56:38 +0100

    v0.3.19: stack alignment validation with elastix alignment: exposed elastix parameters

--- squirrel/library/argument_parsing.py
+++ squirrel/library/argument_parsing.py
@@ -1,15 +1,32 @@
 
 def column_separated_inputs_to_dict(inputs):
-
     if inputs is None:
         return None
 
-    if type(inputs) == list:
-        out_dict = dict()
-        for inp in inputs:
-            key, value = str.split(inp, ':')
-            out_dict[key] = value
+    if not isinstance(inputs, list):
+        raise TypeError(
+            f'Inputs must be either None or list of strings. Found type(inputs) = {type(inputs)}'
+        )
 
-        return out_dict
+    def parse_value(value):
+        # try int
+        try:
+            return int(value)
+        except ValueError:
+            pass
 
-    raise TypeError(f'Inputs must be either None or list of string. Found type(inputs) = {type(inputs)}')
+        # try float
+        try:
+            return float(value)
+        except ValueError:
+            pass
+
+        # fallback to string
+        return value
+
+    out_dict = {}
+    for inp in inputs:
+        key, value = inp.split(':', 1)  # split only on first colon
+        out_dict[key] = parse_value(value)
+
+    return out_dict
